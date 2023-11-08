bce1f4fa96e223830de9230b2de9de2bc773df11
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-11-07 20:48:36 -0700

    Add initial theme constants

---
+++ constants/theme.js
@@ -0,0 +1,72 @@
+const COLORS = {
+  blue: "#0d6efd",
+  indigo: "#6610f2",
+  purple: "#6f42c1",
+  pink: "#d63384",
+  red: "#dc3545",
+  orange: "#fd7e14",
+  yellow: "#ffc107",
+  green: "#198754",
+  teal: "#20c997",
+  cyan: "#0dcaf0",
+  black: "#000",
+  white: "#fff",
+  gray1: "#f8f9fa",
+  gray2: "#e9ecef",
+  gray3: "#dee2e6",
+  gray4: "#ced4da",
+  gray5: "#adb5bd",
+  gray6: "#6c757d",
+  gray7: "#495057",
+  gray8: "#343a40",
+  gray9: "#212529",
+  primary: "#0d6efd",
+  secondary: "#6c757d",
+  success: "#198754",
+  info: "#0dcaf0",
+  warning: "#ffc107",
+  danger: "#dc3545",
+  light: "#f8f9fa",
+  dark: "#212529",
+};
+
+const FONT = {
+  regular: "DMRegular",
+  medium: "DMMedium",
+  bold: "DMBold",
+  cute: "Cute"
+};
+
+const SIZES = {
+  xSmall: 10,
+  small: 12,
+  medium: 16,
+  large: 20,
+  xLarge: 24,
+  xxLarge: 32,
+};
+
+const SHADOWS = {
+  small: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 2,
+  },
+  medium: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 5.84,
+    elevation: 5,
+  },
+};
+
+export { COLORS, FONT, SIZES, SHADOWS };
