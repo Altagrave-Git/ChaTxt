12541bb811733cdb9c26cb75b99f3eb925da5373
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-11-07 20:46:18 -0700

    Initial avatar asset files

---
+++ assets/avatar/legs.jsx
@@ -0,0 +1,9 @@
+import { Path, G, Use, Defs } from "react-native-svg";
+
+const Legs = [
+    (
+        <G transform=""><G transform=""><Defs><Path fill="#C1D2E2" d="M125.7 0q-.2.05-.35.1Q62.1 12.35.85.2.6.1.35.1.2.05.1.05.05 0 0 0q0 .05.05.1v19.5q.35-.1.7-.15h.1q32.4-5.15 62 2.9 29.7-8.1 62.3-2.85.15 0 .3.05.15 0 .25.05V.05l.05-.05h-.05z" id="LegPan001__Symbol_1_0_Layer0_0_FILL"></Path></Defs><Use xlinkHref="#LegPan001__Symbol_1_0_Layer0_0_FILL" transform="translate(129.65 310.25)" id="LegPan001__col1n"></Use></G></G>
+    )
+]
+
+export default Legs;
---
+++ assets/avatar/mouth.jsx
@@ -0,0 +1,9 @@
+import { Path, Use, G, Defs } from "react-native-svg";
+
+const Mouth = [
+    (
+        <G transform=""><G transform=""><Defs><Path fill="#000000" d="M27.95.55q-.15-.35-.5-.5-.4-.15-.75.05Q14.55 5.7 1.35.1.95-.1.6.1.25.2.1.6q-.2.35 0 .7.1.35.5.55Q14.65 7.8 27.5 1.8q.35-.15.5-.5.15-.4-.05-.75z" id="FacMou001__Symbol_1_0_Layer0_0_FILL"></Path></Defs><G id="FacMou001__col1n" transform="translate(178.5 224.8)"><Use xlinkHref="#FacMou001__Symbol_1_0_Layer0_0_FILL" id="FacMou001__col1n_FL"></Use></G></G></G>
+    )
+]
+
+export default Mouth;
