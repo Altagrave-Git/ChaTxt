7de31567376d0cedb5313a34b1cbc441d001dd2d
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-11-10 01:33:11 -0700

    Add email, pass validation, auth call placeholder

--- app/auth/login.jsx
+++ app/auth/login.jsx
@@ -3,13 +3,32 @@ import { View, Text, TextInput, ScrollView, SafeAreaView, Pressable } from "reac
 import { useRouter, Stack } from "expo-router";
 import { COLORS, SIZES, FONT, icons } from "../../constants";
 import styles from "../../styles/auth";
+import ToonAPI from "../../api/api";
+import validator from "../../utils/validator";
 
 const LoginView = () => {
   const router = useRouter();
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [message, setMessage] = useState(null);
 
+  const handleLogin = async () => {
+    setMessage(null);
+    if (!validator.email(email)) {
+      setMessage("Enter valid e-mail address.");
+    } else if (!validator.password(password)) {
+      setMessage("Password must be 8+ characters with atleast one uppercase, lowercase, numeric and special character.");
+    } else {
+      const formData = new FormData();
+      formData.append("email", email);
+      formData.append("password", password);
+
+      ToonAPI.post("/users/login/", formData)
+        .then(data => console.log(data))
+    }
+    return
+  }
 
   return (
     <SafeAreaView style={{
@@ -19,39 +38,58 @@ const LoginView = () => {
       <Stack.Screen options={{
         headerStyle: { backgroundColor: COLORS.warning },
         headerShadowVisible: true,
-        headerLeft: () => (
-          <Text style={{ fontFamily: FONT.cute, fontSize: 40, color: COLORS.orange }}>Login</Text>
+        headerLeft: () => <Text style={{ fontFamily: FONT.cute, fontSize: 36, color: COLORS.light }}>Login</Text>,
+        headerRight: () => (
+          <Pressable onPress={() => router.push("/auth/register/")}>
+            <Text style={{ fontFamily: FONT.cute, fontSize: 36, color: COLORS.secondary }}>Register</Text>
+          </Pressable>
         ),
         headerTitle: ""
       }} />
 
-      <View style={styles.controlContainer}>
-        <View style={styles.controlWrapper}>
-          <TextInput
-            style={styles.controlInput}
-            value={email}
-            onChange={(e) => setEmail(e.value)}
-            placeholder="E-mail address"
-            placeholderTextColor={COLORS.placeholder}
-          />
+      <ScrollView>
+        <View style={styles.controlContainer}>
+          { message != null &&
+            <Text style={{
+              color: COLORS.danger,
+              fontFamily: FONT.regular,
+              fontSize: 16,
+              textAlign: "center"
+            }}>{message}</Text>
+          }
+
+          <View style={styles.controlWrapper}>
+            <TextInput
+              style={styles.controlInput}
+              value={email}
+              onChangeText={(text) => setEmail(text.trim())}
+              placeholder="E-mail address"
+              placeholderTextColor={COLORS.placeholder}
+            />
+          </View>
+          <View style={styles.controlWrapper}>
+            <TextInput
+              style={styles.controlInput}
+              value={password}
+              onChangeText={(text) => setPassword(text.trim())}
+              placeholder="Password"
+              secureTextEntry={true}
+              placeholderTextColor={COLORS.placeholder}
+            />
+          </View>
+
+          <Pressable 
+            style={styles.controlButton}
+            onPress={handleLogin}
+          >
+            <Text style={{
+              color: COLORS.warning,
+              fontFamily: FONT.cute,
+              fontSize: 30
+            }}>Login</Text>
+          </Pressable>
         </View>
-        <View style={styles.controlWrapper}>
-          <TextInput
-            style={styles.controlInput}
-            value={password}
-            onChange={(e) => setPassword(e.value)}
-            placeholder="Password"
-            passwordRules="required: upper; required: lower; required: digit; minlength: 8"
-            secureTextEntry={true}
-            placeholderTextColor={COLORS.placeholder}
-          />
-        </View>
-        <Pressable 
-          style={styles.controlButton}
-        >
-          <Text style={styles.buttonText}>Login</Text>
-        </Pressable>
-      </View>
+      </ScrollView>
     </SafeAreaView>
   )
 }
