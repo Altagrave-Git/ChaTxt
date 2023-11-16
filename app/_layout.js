8976248e46017930fa8bfa918c757845931c176b
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-11-16 01:33:16 -0700

    Add global context wrapper

--- app/_layout.js
+++ app/_layout.js
@@ -1,30 +1,11 @@
-import { Stack } from "expo-router";
-import { useCallback } from "react";
-import { useFonts } from 'expo-font';
-import * as SplashScreen from 'expo-splash-screen';
+import { Slot } from "expo-router";
+import { GlobalStateProvider } from "../context/global";
 
-SplashScreen.preventAutoHideAsync();
+export default Root = () => {
 
-const Layout = () => {
-    const [fontsLoaded] = useFonts({
-        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
-        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
-        DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
-        Cute: require('../assets/fonts/Cute.ttf'),
-    });
-
-    const onLayoutRootView = useCallback(async () => {
-        if (fontsLoaded) {
-            await SplashScreen.hideAsync();
-        }
-    }, [fontsLoaded])
-
-    if (!fontsLoaded) {
-        return null;
-    }
-
-    return <Stack onLayout={onLayoutRootView} />;
+  return (
+    <GlobalStateProvider>
+      <Slot />
+    </GlobalStateProvider>
+  );
 }
-
-
-export default Layout;
