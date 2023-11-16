448b1a5c6d240aaaee001121d5688d4fcc7247e8
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-11-16 01:34:07 -0700

    Create global context provider, hook

--- context/global.js
+++ context/global.js
@@ -1,11 +1,39 @@
-import { createContext, useContext, useReducer } from 'react';
+import React, { createContext, useContext, useReducer } from 'react';
 
 const AppContext = createContext();
 
 const initial = {
-
+  user: null,
+  theme: 'default'
 }
 
-export const GlobalState = ({children}) => {
-  const [globalState, setGlobalState] = useState();
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "user":
+      return { ...state, user: action.payload }
+
+    case "theme":
+      return { ...state, theme: action.payload }
+
+    default:
+      return state;
+  }
+}
+
+export const GlobalStateProvider = ({children}) => {
+  const [state, dispatch] = useReducer(reducer, initial);
+
+  return (
+    <AppContext.Provider value={{ state, dispatch }}>
+      {children}
+    </AppContext.Provider>
+  )
+}
+
+export const useGlobalState = () => {
+  const context = useContext(AppContext);
+  if (!context) {
+    throw new Error('useGlobalState must be used within a GlobalStateProvider')
+  }
+  return context;
 }
