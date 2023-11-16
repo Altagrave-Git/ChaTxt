988382d08ce31be3bb66ba22c33f4812b074627a
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-11-16 01:32:36 -0700

    Force redirect if not authorized

---
+++ app/(app)/_layout.js
@@ -0,0 +1,37 @@
+import { Stack, Redirect } from "expo-router";
+import { useCallback } from "react";
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useGlobalState } from "../../context/global";
+
+SplashScreen.preventAutoHideAsync();
+
+export default Layout = () => {
+  const { state } = useGlobalState();
+
+  const [fontsLoaded] = useFonts({
+    Bold: require('../../assets/fonts/DMSans-Bold.ttf'),
+    Medium: require('../../assets/fonts/DMSans-Medium.ttf'),
+    Regular: require('../../assets/fonts/DMSans-Regular.ttf'),
+    Cute: require('../../assets/fonts/Cute.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    await SplashScreen.hideAsync();
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <>
+      { state.user !== null ? (
+          <Stack onLayout={onLayoutRootView} />
+        ) : (
+          <Redirect href={"/login/"} />
+        )
+      }
+    </>
+  );
+}
