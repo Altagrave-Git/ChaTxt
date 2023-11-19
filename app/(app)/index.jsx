97631942c0a51e51dfd95a01fc793972bd64b9f2
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-11-18 22:57:22 -0700

    Add user session and theme to home view

--- app/(app)/index.jsx
+++ app/(app)/index.jsx
@@ -3,27 +3,29 @@ import { useRouter, Stack } from "expo-router";
 import { COLORS, SIZES, FONT, icons } from "../../constants";
 import Svg, { Path } from "react-native-svg";
 import Avatar from "../../components/avatar/avatar";
-import { useGlobalState } from "../../context/global";
+import { useSession } from "../../global/session";
+import { useTheme } from "../../global/theme";
 
 const Home = () => {
-  const { state } = useGlobalState();
+  const { session } = useSession();
+  const { theme } = useTheme();
 
   const router = useRouter();
 
   return (
     <SafeAreaView style={{
       flex: 1,
-      backgroundColor: COLORS.gray1
+      backgroundColor: theme.background
     }}>
       <Stack.Screen options={{
-        headerStyle: { backgroundColor: COLORS.warning },
+        headerStyle: { backgroundColor: theme.primary, },
         headerShadowVisible: true,
         headerLeft: () => (
-          <Text style={{ fontFamily: FONT.cute, fontSize: 40, color: COLORS.orange }}>ClubToon</Text>
+          <Text style={{ fontFamily: FONT.cute, fontSize: 40, color: theme.secondary }}>ClubToon</Text>
         ),
         headerRight: () => (
           <Pressable>
-            <Svg style={{fill: COLORS.orange}} xmlns="http://www.w3.org/2000/svg" height={25} width={20} viewBox="0 0 448 512">
+            <Svg style={{fill: theme.secondary}} xmlns="http://www.w3.org/2000/svg" height={25} width={20} viewBox="0 0 448 512">
               <Path d="M0 96c0-17.7 14.3-32 32-32h384c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zm0 160c0-17.7 14.3-32 32-32h384c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zm448 160c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32h384c17.7 0 32 14.3 32 32z"/>
             </Svg>
           </Pressable>
@@ -35,8 +37,21 @@ const Home = () => {
 
       </ScrollView>
       <View style={{ position: 'absolute', bottom: 20, right: 20 }}>
-        <Pressable style={{height: 75, width: 75, borderColor: COLORS.warning, borderStyle: "solid", borderWidth: 2, borderRadius: 50,
-        backgroundColor: COLORS.danger }} onPress={() => router.push('/login/')}>
+        <Pressable 
+          style={{
+            height: 75,
+            width: 75,
+            borderColor: theme.primary,
+            borderStyle: "solid",
+            borderWidth: 2,
+            borderRadius: 50,
+            backgroundColor: COLORS[session.user.color],
+            shadowColor: COLORS.black,
+            shadowOpacity: 1,
+            shadowRadius: 3,
+            elevation: 5
+          }} onPress={() => router.push('/login/')}
+        >
           <Avatar />
         </Pressable>
       </View>
