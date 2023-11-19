6972ce5c9f5c18ba7c0e4bd61ac0225a460d5ca2
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-11-18 22:54:50 -0700

    Add b1 mouth list function

---
+++ assets/avatar/mouth/types/B/1.js
@@ -0,0 +1,7 @@
+import { Path, Use, G, Defs } from "react-native-svg";
+
+export default mouthTypeB1 = (colors=[]) => [
+  (
+      <G transform=""><G class="active-stage-item" transform=""><Defs><Path fill={colors[0]} d="M5.7 7.2q2.7 1.25 5 1.45.65 0 1.25.05v-.05h.35q2.15-.15 4.75-1.35Q19.8 6.05 23 3.6q-6.85-6.45-11.45-1.15Q6.95-2.95 0 3.6q3 2.3 5.7 3.6z" id="FacMou049__Symbol_74_0_Layer0_0_FILL"></Path></Defs><Use xlinkHref="#FacMou049__Symbol_74_0_Layer0_0_FILL" transform="translate(181 220.6)" id="FacMou049__col1n"></Use></G></G>
+  ),
+];
