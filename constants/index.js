e4a30f942ce97c32c481e7b86bee653d991ea40f
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-11-18 23:00:16 -0700

    export user themes constant

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
 import icons from "./icons";
-import { COLORS, FONT, SIZES, SHADOWS } from "./theme";
+import { COLORS, FONT, SIZES, SHADOWS, THEMES } from "./theme";
 
-export { icons, COLORS, FONT, SIZES, SHADOWS };
+export { icons, COLORS, FONT, SIZES, SHADOWS, THEMES };
