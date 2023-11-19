15aabd1537c787d608c2eebe282bd5a5423a1fc8
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-11-18 23:03:22 -0700

    Create color scheme context provider

---
+++ global/theme.js
@@ -0,0 +1,28 @@
+import { createContext, useContext } from 'react';
+import { useStorageState } from "./store";
+import { THEMES } from "../constants/theme";
+
+const ThemeContext = createContext();
+
+export function ThemeProvider({children}) {
+  const [[loadingTheme, theme], setTheme] = useStorageState('theme');
+
+  return (
+    <ThemeContext.Provider
+      value={{
+        setTheme: (themeChoice) => setTheme(THEMES[themeChoice]),
+        theme,
+        loadingTheme
+    }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+}
+
+export function useTheme() {
+  const context = useContext(ThemeContext);
+  if (!context) {
+    throw new Error('useTheme must be wrapped in a <ThemeProvider />');
+  }
+  return context;
+}
