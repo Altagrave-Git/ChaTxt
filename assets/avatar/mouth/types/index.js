fa52778fb1e323a7bbeec1e58a6bab8dbfe3805b
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-11-18 22:59:23 -0700

    Export eyes list functions as nested object

---
+++ assets/avatar/mouth/types/index.js
@@ -0,0 +1,13 @@
+import mouthTypeA0 from "./A/0";
+import mouthTypeB0 from "./B/0";
+import mouthTypeB1 from "./B/1";
+
+export const mouthTypes = {
+  A: [
+    mouthTypeA0
+  ],
+  B: [
+    mouthTypeB0,
+    mouthTypeB1
+  ]
+}
