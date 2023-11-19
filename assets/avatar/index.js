258bec61f30b6a6ff24d2a7cedcd1ec8d484eebc
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-11-18 22:52:24 -0700

    Export all avatar components

---
+++ assets/avatar/index.js
@@ -0,0 +1,22 @@
+import Hair from "./hair";
+import Torso from "./torso";
+import Legs from "./legs";
+import Hands from "./hands";
+import Feet from "./feet";
+
+import { Skin, skinColors } from "./skin/skin";
+import { Eyes, eyesColors } from "./eyes";
+
+import { Mouth } from "./mouth/mouth";
+
+
+export {
+  Skin,
+  Hair,
+  Eyes,
+  Mouth,
+  Torso,
+  Legs,
+  Hands,
+  Feet
+}
