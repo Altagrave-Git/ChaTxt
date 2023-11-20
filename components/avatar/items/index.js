d35f3baeaeaa0cb4fe7fea6bcc9ebf5f824bbf83
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-11-20 09:16:24 -0700

    Export all avatar component files in nested object

---
+++ components/avatar/items/index.js
@@ -0,0 +1,19 @@
+import { skin } from "./skin";
+import { hair } from "./hair";
+import { eyes } from "./eyes";
+import { mouth } from "./mouth";
+import { torso } from "./torso";
+import { hands } from "./hands";
+import { legs } from "./legs";
+import { feet } from "./feet";
+
+export const avatarItems = {
+  skin,
+  hair,
+  eyes,
+  mouth,
+  torso,
+  hands,
+  legs,
+  feet
+}
