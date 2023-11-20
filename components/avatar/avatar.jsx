b8816aef94a42f39c5f7a4bee43233dacbc97f91
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-11-20 09:21:51 -0700

    Construct svg avatars from user data, incl. test

--- components/avatar/avatar.jsx
+++ components/avatar/avatar.jsx
@@ -1,24 +1,27 @@
 import Svg from "react-native-svg";
-import { Hair, Legs, Hands, Feet } from "../../assets/avatar";
-import { Skin } from "../../assets/avatar/skin/skin";
-import { Eyes } from "../../assets/avatar/eyes";
-import { Torso } from "../../assets/avatar/torso/torso";
-import { Mouth } from "../../assets/avatar";
+import AvatarItem from "./avataritem";
+
+
+
+const avatar = {
+  skin: { type: "A", name: "skin_A2_0", colors: ["#FFE69A", "#F0B800"] },
+  hair: { type: "A", name: "hair_A0_0", colors: [] },
+  eyes: { type: "A", name: "eyes_A0_0", colors: [], },
+  mouth: { type: "B", name: "mouth_B0_0", colors: [] },
+  torso: { type: "A", name: "torso_A0_1", colors: [] },
+  hands: { type: "B", name: "hands_B1_0", colors: ["#2289aa"] },
+  legs: { type: "A", name: "legs_A1_0", colors: [] },
+  feet: { type: "A", name: "feet_A1_0", colors: ["#888"] }
+}
 
 
 const Avatar = ({ skin={}, hair={}, eyes={}, mouth={}, torso={}, hands={}, legs={}, feet={} }) => {
-    return (
-        <Svg id="stage-svg" preserveAspectRatio="none" viewBox="0 0 385 385">
-            {Skin(0, 0, 0, 0)}
-            {Hair[0]}
-            {Eyes(0, 0, 0, 0)}
-            {Mouth("A", 0, [], "crooked")}
-            {Torso(0, 0, 0, 0)}
-            {Legs[0]}
-            {Hands[0]}
-            {Feet[0]}
-        </Svg>
-    )
+
+  return (
+    <Svg id="stage-svg" preserveAspectRatio="none" viewBox="0 0 385 385">
+      { Object.keys(avatar).map((category, index) => <AvatarItem category={category} item={avatar[category]} key={index} />) }
+    </Svg>
+  )
 }
 
 export default Avatar;
