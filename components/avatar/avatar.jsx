1583e8c97edcb90e8f3e5f9a62aa9bdf55b5b854
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-11-25 10:42:45 -0700

    Modify avatar test values

--- components/avatar/avatar.jsx
+++ components/avatar/avatar.jsx
@@ -1,24 +1,23 @@
-import Svg from "react-native-svg";
+import { Svg, G, Use, Path, Defs } from "react-native-svg";
 import AvatarItem from "./avataritem";
 
 
-
-const avatar = {
-  skin: { type: "A", name: "skin_A2_0", colors: ["#FFE69A", "#F0B800"] },
-  hair: { type: "A", name: "hair_A0_0", colors: [] },
-  eyes: { type: "A", name: "eyes_A0_0", colors: [], },
-  mouth: { type: "B", name: "mouth_B0_0", colors: [] },
-  torso: { type: "A", name: "torso_A0_1", colors: [] },
-  hands: { type: "B", name: "hands_B1_0", colors: ["#2289aa"] },
-  legs: { type: "A", name: "legs_A1_0", colors: [] },
-  feet: { type: "A", name: "feet_A1_0", colors: ["#888"] }
+const avatarTest = {
+  skin: { type: "A", name: "skin_A_0", colors: ["#FFE69A", "#F0B800"] },
+  hair: { type: "A", name: "hair_A_2", colors: ["#FFDD22"] },
+  eyes: { type: "A", name: "eyes_A_2", colors: [], },
+  mouth: { type: "B", name: "mouth_B_1", colors: [] },
+  torso: { type: "A", name: "torso_A_1", colors: [] },
+  hands: { type: "A", name: "hands_A_0", colors: [] },
+  legs: { type: "A", name: "legs_A_0", colors: [] },
+  feet: { type: "A", name: "feet_A_0", colors: ["#888"] }
 }
 
 
-const Avatar = ({ skin={}, hair={}, eyes={}, mouth={}, torso={}, hands={}, legs={}, feet={} }) => {
+const Avatar = ({ avatar=avatarTest }) => {
 
   return (
-    <Svg id="stage-svg" preserveAspectRatio="none" viewBox="0 0 385 385">
+    <Svg id="stage-svg" preserveAspectRatio="xMidYMid meet" viewBox="0 0 385 385">
       { Object.keys(avatar).map((category, index) => <AvatarItem category={category} item={avatar[category]} key={index} />) }
     </Svg>
   )
