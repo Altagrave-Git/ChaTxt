47f4573152c6672ad705f8d5755a4499a5640e76
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-11-25 10:40:22 -0700

    Export all avatar methods

---
+++ components/avatar/index.js
@@ -0,0 +1,11 @@
+import Avatar from "./avatar";
+import { adjustColor } from "./shade";
+import AvatarItem from "./avataritem";
+import { SKINCOLOR } from "./avatar.styles";
+
+export {
+  Avatar,
+  adjustColor,
+  AvatarItem,
+  SKINCOLOR
+}
