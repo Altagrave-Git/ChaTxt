f3d0faf01d4949dde4f214c3f5cf758972a4bfea
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-11-30 17:32:52 -0700

    Add item type keyword, move color function to obj

--- components/avatar/items/feet/A.js
+++ components/avatar/items/feet/A.js
@@ -1,26 +1,28 @@
 import { Path, G, Defs, Use } from "react-native-svg";
 
-const feetTypeA = (colors) => ({
+const feetTypeA = {
   // COLOR SCHEME 0
   feet_A_0: {
-    obj: (
+    obj: (colors) => (
       <></>
     ),
     M: true,
     F: true,
-    scheme: 0
+    scheme: 0,
+    type: 'A'
   },
 
 
   // COLORS SCHEME 1
   feet_A_1: {
-    obj: (
+    obj: (colors) => (
       <G transform=""><G transform=""><Defs><Path fill={colors[0]} d="M146.9 7.7q-9-5.25-23.3-6.95-13.1-1.45-27.4 0-7 .8-14.2 2.45l-8.3 2.05-8.3-2.05Q58.2 1.55 51.2.75 36.9-.7 23.8.75 9.1 2.5 0 8h147.4l-.5-.3z" id="LegSho001__Symbol_2_0_Layer0_0_FILL"></Path></Defs><Use xlinkHref="#LegSho001__Symbol_2_0_Layer0_0_FILL" transform="translate(118.8 326)" id="LegSho001__col1n"></Use></G></G>
     ),
     M: true,
     F: true,
-    scheme: 1
+    scheme: 1,
+    type: 'A'
   },
-})
+}
 
 export default feetTypeA;
--- components/avatar/items/hands/A.js
+++ components/avatar/items/hands/A.js
@@ -1,15 +1,16 @@
 import { Path, G, Defs, Use } from "react-native-svg"
 
-const handsTypeA = (colors) => ({
+const handsTypeA = {
   // COLOR SCHEME 0
   hands_A_0: {
-    obj: (
+    obj: (colors) => (
       <></>
     ),
     M: true,
     F: true,
-    scheme: 0
+    scheme: 0,
+    type: "A"
   },
-})
+}
 
 export default handsTypeA;
