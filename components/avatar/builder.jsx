e4c59493f7cc8f463260539659a77309b2aa8a1e
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-12-01 04:12:34 -0700

    Mk setMale, setFemale, setCat., setPrev, setNext++

--- components/avatar/builder.jsx
+++ components/avatar/builder.jsx
@@ -2,7 +2,7 @@ import Avatar from "./avatar";
 import { avatarItems } from "./items";
 import { useReducer } from "react";
 import { COLORS } from "../../constants";
-import { SKINCOLORS } from "./avatar.styles";
+import { SKINCOLOR } from "./avatar.styles";
 import { getAvatarItemSet, baseAvatar } from "./avatar";
 
 const baseMale = baseAvatar('M');
@@ -17,30 +17,62 @@ const initial = {
   choicesF: femaleItemChoices,
   gender: 'M',
   category: Object.keys(maleItemChoices)[0],
-  item: null
-}
+  itemIndex: Object.values(maleItemChoices)[0].findIndex(obj => obj.name === baseMale[Object.keys(maleItemChoices)[0]].name)
+};
+
+const calculateNewIndex = (currentIndex, itemCount, increment=true) => {
+  if (itemCount <= 1) { return 0 }
+
+  const direction = increment ? 1 : -1;
+  let newIndex = currentIndex + direction;
+
+  if (newIndex < 0) { newIndex = itemCount - 1 }
+  else if (newIndex >= itemCount) { newIndex = 0 }
+  return newIndex;
+};
 
 const reducer = (state, action) => {
+  let index, itemCount, newIndex, choices, initItemIndex, avatar, category;
+
   switch (action.type) {
     case 'setMale':
-      return { ...state, gender: 'M' }
+      initItemIndex = state.choicesM[state.category].findIndex(obj => obj.name === state.avatarM[state.category].name);
+      return { ...state, gender: 'M', itemIndex: initItemIndex }
 
     case 'setFemale':
-      return {...state, gender: 'F' }
+      initItemIndex = state.choicesF[state.category].findIndex(obj => obj.name === state.avatarF[state.category].name);
+      return {...state, gender: 'F', itemIndex: initItemIndex }
 
     case 'setCategory':
-      return {...state, category: action.payload}
+      category = action.payload;
+      avatar = state[`avatar${state.gender}`];
+      choices = state[`choices${state.gender}`];
+      initItemIndex = choices[category].findIndex(obj => obj.name === state[`avatar${state.gender}`][category].name);
+      return {...state, category: action.payload, itemIndex: initItemIndex}
 
-    case 'setItem':
-      const category = Object.keys(action.payload)[0];
-      const item = Object.values(action.payload)[0];
-      const newState = { ...state };
-      if (state.gender === 'M') {
-        newState.avatarM[category] = item
-      } else {
-        newState.avatarF[category] = item
-      }
-      return newState;
+    case 'prevItem':
+      index = state.itemIndex;
+      itemCount = state.gender === 'M' ? state.choicesM[state.category].length : state.choicesF[state.category].length;
+      newIndex = calculateNewIndex(index, itemCount, false)
+
+      return {...state, itemIndex: newIndex}
+
+    case 'nextItem':
+      index = state.itemIndex;
+      itemCount = state.gender === 'M' ? state.choicesM[state.category].length : state.choicesF[state.category].length;
+      newIndex = calculateNewIndex(index, itemCount, true)
+      return {...state, itemIndex: newIndex}
+
+    // case 'equipItem':
+    //   const category = Object.keys(action.payload)[0];
+    //   const item = Object.values(action.payload)[0];
+    //   const newState = { ...state };
+    //   if (state.gender === 'M') {
+    //     newState.avatarM[category] = item
+    //   } else {
+    //     newState.avatarF[category] = item
+    //   }
+    //   return newState;
 
     default: return state;
   }
@@ -52,17 +84,20 @@ export const AvatarBuilder = () => {
   const setMale = () => dispatch({type: 'setMale'});
   const setFemale = () => dispatch({type: 'setFemale'});
   const setCategory = (payload) => dispatch({type: 'setCategory', payload: payload})
-  const setItem = (payload) => dispatch({type: 'setItem', payload: payload});
+  const prevItem = () => dispatch({type: 'prevItem'});
+  const nextItem = () => dispatch({type: 'nextItem'});
+  // const equipItem = (payload) => dispatch({type: 'equipItem', payload: payload});
 
   const gender = state.gender;
 
-  const [avatar, choices, category] = state.gender === 'M' ? [state.avatarM, state.choicesM, state.category] : [state.avatarF, state.choicesF, state.category];
+  const [avatar, choices, category, itemIndex] = state.gender === 'M' ? [state.avatarM, state.choicesM, state.category, state.itemIndex] : [state.avatarF, state.choicesF, state.category, state.itemIndex];
 
   const categories = Object.keys(choices);
-  console.log(choices[category]);
-  console.log(category);
   const items = choices[category];
-  const initItemIndex = items.findIndex(obj => obj.name === avatar[category].name);
 
-  return { avatar, choices, gender, categories, items, initItemIndex, setMale, setFemale, setCategory, setItem };
+  const pallete = category === 'skin' ? SKINCOLOR : COLORS;
+
+  const item = items[itemIndex];
+
+  return { avatar, choices, gender, categories, category, items, itemIndex, item, pallete, setMale, setFemale, setCategory, prevItem, nextItem };
 }
