7f7ed41354f631d54d071c1996d776713631d53f
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-12-01 04:06:42 -0700

    Mv avatar builder logic, state to builder comp.

--- app/(tutorial)/tutorial.jsx
+++ app/(tutorial)/tutorial.jsx
@@ -13,39 +13,7 @@ import { Tab } from "@rneui/themed";
 const TutorialView = () => {
   const { theme } = useTheme();
   const { session } = useSession();
-  const { avatar, choices, gender, setMale, setFemale, setItem } = AvatarBuilder();
-
-  const categories = Object.keys(choices);
-  
-  const [category, setCategory] = useState(0);
-  const [itemArray, setItemArray] = useState(choices[categories[category]]);
-  const [currentItem, setCurrentItem] = useState(choices[categories[category]].findIndex(obj => obj.name === avatar[categories[category]].name));
-
-  const handlePrev = (array, arrayIndex, setArrayIndex) => {
-    let newIndex;
-    if (arrayIndex > 0 && array.length > 1) {
-      newIndex = arrayIndex - 1;
-    } else if (array.length > 1) {
-      newIndex = array.length - 1;
-    } else {
-      newIndex = 0;
-    }
-    setArrayIndex(newIndex)
-    return newIndex;
-  }
-  
-  const handleNext = (array, arrayIndex, setArrayIndex) => {
-    let newIndex;
-    if (array.length - 1 > arrayIndex && array.length > 1) {
-      newIndex = arrayIndex + 1;
-    } else if (array.length > 1) {
-      newIndex = 0;
-    } else {
-      newIndex = 0;
-    }
-    setArrayIndex(newIndex);
-    return newIndex;
-  }
+  const builder = AvatarBuilder();
 
   return (
     <SafeAreaView style={{flex: 1}}>
@@ -62,7 +30,7 @@ const TutorialView = () => {
         {/* SET GENDER */}
         <View style={{alignItems: "center"}}>
           <View style={[{flexDirection: "row", marginTop: 30, borderRadius: 15, width: 160, height: 65, padding: 1}, SHADOWS.medium]}>
-            <Pressable style={{flexGrow: 1}} onPress={setMale} disabled={gender === 'M' ? true : false}>
+            <Pressable style={{flexGrow: 1}} onPress={builder.setMale} disabled={builder.gender === 'M' ? true : false}>
               {({pressed}) => (
                 <View style={{
                   paddingHorizontal: 10,
@@ -72,15 +40,15 @@ const TutorialView = () => {
                   alignItems: "center",
                   height: "100%",
                   borderWidth: 4,
-                  backgroundColor: gender === 'M' || pressed ? COLORS.blue : COLORS.dkBlue,
-                  borderColor: pressed || gender === 'M' ? 'transparent' : COLORS.blue
+                  backgroundColor: builder.gender === 'M' || pressed ? COLORS.blue : COLORS.dkBlue,
+                  borderColor: pressed || builder.gender === 'M' ? 'transparent' : COLORS.blue
                 }}>
                   <Foundation name="male-symbol" size={50} color={COLORS.ltBlue} />
                 </View>
               )}
             </Pressable>
 
-            <Pressable style={{flexGrow: 1}} onPress={setFemale} disabled={gender === 'F' ? true : false}>
+            <Pressable style={{flexGrow: 1}} onPress={builder.setFemale} disabled={builder.gender === 'F' ? true : false}>
               {({pressed}) => (
                 <View style={{
                   paddingHorizontal: 10,
@@ -90,8 +58,8 @@ const TutorialView = () => {
                   justifyContent: "center",
                   alignItems: "center",
                   height: "100%",
-                  backgroundColor: gender === 'F' || pressed ? COLORS.pink : COLORS.dkPink,
-                  borderColor: pressed || gender === 'F' ? "transparent" : COLORS.pink,
+                  backgroundColor: builder.gender === 'F' || pressed ? COLORS.pink : COLORS.dkPink,
+                  borderColor: pressed || builder.gender === 'F' ? "transparent" : COLORS.pink,
                 }}>
                   <Foundation name="female-symbol" size={50} color={COLORS.ltPink} />
                 </View>
@@ -102,47 +70,39 @@ const TutorialView = () => {
 
         {/* RENDER AVATAR */}
         <View style={{width: "100%", maxHeight: 300}}>
-          <Avatar avatar={{...avatar, [categories[category]]: avatar[categories[category]]}} />
+          <Avatar avatar={{...builder.avatar, [builder.category]: builder.avatar[builder.category]}} />
         </View>
 
 
         <View style={{flexDirection: "row", justifyContent: "space-evenly", width: "100%", display: "none"}}>
           <View style={{width: "50%", alignItems: "center"}}>
-            <Pressable onPress={() => {
-              const newCategory = handlePrev(categories, category, setCategory);
-              setItemArray(choices[categories[newCategory]]);
-              setCurrentItem(0);
-            }}>
+            <Pressable onPress={null}>
               <AntDesign name="caretup" size={40} color={theme.primary} />
             </Pressable>
             
-            <Text style={{fontSize: 40, fontFamily: FONT.cute}}>{categories[category][0].toUpperCase() + categories[category].slice(1)}</Text>
+            <Text style={{fontSize: 40, fontFamily: FONT.cute}}>{builder.category[0].toUpperCase() + builder.category.slice(1)}</Text>
 
-            <Pressable onPress={() => {
-              const newCategory = handleNext(categories, category, setCategory);
-              setItemArray(choices[categories[newCategory]]);
-              setCurrentItem(0);
-            }}>
+            <Pressable onPress={null}>
               <AntDesign name="caretdown" size={40} color={theme.primary} />
             </Pressable>
           </View>
 
           <View style={{width: "50%", alignItems: "center"}}>
-            <Pressable onPress={() => handlePrev(itemArray, currentItem, setCurrentItem)}>
+            <Pressable onPress={builder.prevItem}>
               <AntDesign name="caretup" size={40} color={theme.primary} />
             </Pressable>
 
-            <Text style={{fontSize: 40, fontFamily: FONT.cute}}>{currentItem}</Text>
+            <Text style={{fontSize: 40, fontFamily: FONT.cute}}>{builder.itemIndex}</Text>
 
-            <Pressable onPress={() => handleNext(itemArray, currentItem, setCurrentItem)}>
+            <Pressable onPress={builder.nextItem}>
               <AntDesign name="caretdown" size={40} color={theme.primary} />
             </Pressable>
           </View>
         </View>
       </ScrollView>
       <Tab
-        value={category}
-        onChange={(e) => setCategory(e)}
+        value={builder.categories.indexOf(builder.category)}
+        onChange={(e) => builder.setCategory(builder.categories[e])}
         indicatorStyle={{
           backgroundColor: COLORS.dkGreen,
           height: 3,
@@ -152,10 +112,10 @@ const TutorialView = () => {
         style={{backgroundColor: theme.primary, overflow: "hidden", maxWidth: 992}}
         variant="default"
       >
-        { categories.map((item, index) => (
+        { builder.categories.map((val, index) => (
           <Tab.Item
             key={index}
-            icon={(active) => <AvatarIcon category={item} color={active ? COLORS.green : theme.highlight} style={{width: "100%", height: 30}} />}
+            icon={(active) => <AvatarIcon category={val} color={active ? COLORS.green : theme.highlight} style={{width: "100%", height: 30}} />}
             buttonStyle={(active) => ({
               paddingVertical: 12
             })}
