aa2683e165c458834e98cfa1196eb5da4a18a9c2
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-11-30 17:51:05 -0700

    Mk funcs: getavataritemset, baseavatar (details)
    getAvatarItemSet():
    -- transform avatar's default complex nested data structure
    (categ. -> type -> scheme -> name -> meta -> item(colors))
    to simple nested strucure for use in avatar builder state
    (avataritems -> categories -> obj||array -> item(colors))
    
    baseAvatar():
    -- construct male and female base avatar pointers with first usable
    item of each item category to be rendered with Avatar() component

--- components/avatar/avatar.jsx
+++ components/avatar/avatar.jsx
@@ -1,20 +1,68 @@
-import { Svg, G, Use, Path, Defs } from "react-native-svg";
-import AvatarItem from "./avataritem";
+import { Svg } from "react-native-svg";
+import { avatarItems } from "./items";
+import { SKINCOLOR } from "./avatar.styles";
+import { COLORS } from "../../constants";
 
 
-const avatarTest = {
-  skin: { type: "A", name: "skin_A_0", colors: ["#FFE69A", "#F0B800"] },
-  hair: { type: "A", name: "hair_A_2", colors: ["#FFDD22"] },
-  eyes: { type: "A", name: "eyes_A_2", colors: [], },
-  mouth: { type: "B", name: "mouth_B_1", colors: [] },
-  torso: { type: "A", name: "torso_A_1", colors: [] },
-  hands: { type: "A", name: "hands_A_0", colors: [] },
-  legs: { type: "A", name: "legs_A_0", colors: [] },
-  feet: { type: "A", name: "feet_A_0", colors: ["#888"] }
+export const getAvatarItemSet = (repr='object') => {
+  const avatarCategories = Object.entries(avatarItems);
+  const maleItemChoices = {};
+  const femaleItemChoices = {};
+  avatarCategories.forEach((category) => {
+    const maleChoices = {};
+    const femaleChoices = {};
+    Object.values(category[1]).forEach((item) => {
+      Object.keys(item).forEach((entry) => {
+        if (item[entry].M) { maleChoices[entry] = item[entry] };
+        if (item[entry].F) { femaleChoices[entry] = item[entry] };
+      })
+    });
+    maleItemChoices[category[0]] = maleChoices;
+    femaleItemChoices[category[0]] = femaleChoices;
+  });
+
+  if (repr === 'array') {
+    const allChoices = [maleItemChoices, femaleItemChoices]
+    allChoices.forEach((choices, index) => {
+      Object.keys(choices).forEach((key) => {
+        const list = [];
+        const itemNames = Object.keys(choices[key]);
+        Object.values(choices[key]).forEach((value, index) => {
+          value['name'] = itemNames[index];
+          list.push(value);
+        })
+        index ? femaleItemChoices[key] = list : maleItemChoices[key] = list;
+      })
+    })
+  }
+  return { maleItemChoices, femaleItemChoices };
 }
 
+export const baseAvatar = (gender='M') => {
+  const { maleItemChoices, femaleItemChoices } = getAvatarItemSet();
+  const itemChoices = gender === 'M' ? maleItemChoices : femaleItemChoices;
+  const avatar = {};
+  Object.keys(itemChoices).forEach((category) => {
+    const vals = {};
+    const pallete = category === 'skin' ? Object.values(SKINCOLOR) : Object.values(COLORS);
+    const scheme = Object.values(itemChoices[category])[0].scheme;
+    const colors = [];
+    for (let i = 0; i < scheme; i++) {
+      colors.push(pallete[Math.round(Math.round((pallete.length - 1) * Math.ceil(Math.random() * 100))/100)])
+    }
+    vals['name'] = Object.keys(itemChoices[category])[0];
+    vals['type'] = Object.values(itemChoices[category])[0].type;
+    vals['colors'] = category === 'skin' ? pallete[0] : colors;
+    avatar[category] = vals;
+  })
+  return avatar;
+}
 
-const Avatar = ({ avatar=avatarTest }) => {
+const testAvatar = baseAvatar();
+
+export const AvatarItem = ({category, item}) => (avatarItems[category][item.type][item.name].obj(item.colors));
+
+const Avatar = ({ avatar=testAvatar }) => {
 
   return (
     <Svg id="stage-svg" preserveAspectRatio="xMidYMid meet" viewBox="0 0 385 385">
