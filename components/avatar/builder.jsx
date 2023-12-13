4b3b316ec69fd3b27e031388c4ed12b019f45251
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-12-12 20:06:24 -0700

    Get or create initial color set, add item equip method

--- components/avatar/builder.jsx
+++ components/avatar/builder.jsx
@@ -1,14 +1,11 @@
-import Avatar from "./avatar";
-import { avatarItems } from "./items";
 import { useReducer } from "react";
-import { COLORS } from "../../constants";
-import { SKINCOLOR } from "./avatar.styles";
 import { getAvatarItemSet, baseAvatar } from "./avatar";
+import { avatarPalettes } from "./items";
 
 const baseMale = baseAvatar('M');
 const baseFemale = baseAvatar('F');
 
-const { maleItemChoices, femaleItemChoices } = getAvatarItemSet('array');
+const { maleItemChoices, femaleItemChoices } = getAvatarItemSet();
 
 const initial = {
   avatarM: baseMale,
@@ -17,7 +14,20 @@ const initial = {
   choicesF: femaleItemChoices,
   gender: 'M',
   category: Object.keys(maleItemChoices)[0],
-  itemIndex: Object.values(maleItemChoices)[0].findIndex(obj => obj.name === baseMale[Object.keys(maleItemChoices)[0]].name)
+  itemIndex: Object.values(maleItemChoices)[0].findIndex(obj => obj.name === baseMale[Object.keys(maleItemChoices)[0]].name),
+  currentColors: (() => {
+    const currentColors = {};
+    Object.entries(baseMale).forEach((entry) => {
+      currentColors[entry[0]] = [...entry[1].colors];
+      const colorCount = entry[1].colors.length;
+      const palette = Object.values(avatarPalettes[entry[0]].default);
+
+      for (let i = colorCount; 3 > i; i++) {
+        currentColors[entry[0]].push(palette[Math.round(Math.round((palette.length - 1) * Math.ceil(Math.random() * 100))/100)]);
+      }
+    });
+    return currentColors;
+  })()
 };
 
 const calculateNewIndex = (currentIndex, itemCount, increment=true) => {
@@ -32,16 +42,14 @@ const calculateNewIndex = (currentIndex, itemCount, increment=true) => {
 };
 
 const reducer = (state, action) => {
-  let index, itemCount, newIndex, choices, initItemIndex, avatar, category;
+  let itemCount, newIndex, choices, initItemIndex, avatar, category;
 
   switch (action.type) {
     case 'setMale':
-      initItemIndex = state.choicesM[state.category].findIndex(obj => obj.name === state.avatarM[state.category].name);
-      return { ...state, gender: 'M', itemIndex: initItemIndex }
+      return { ...state, gender: 'M', itemIndex: state.choicesM[state.category].findIndex(obj => obj.name === state.avatarM[state.category].name) }
 
     case 'setFemale':
-      initItemIndex = state.choicesF[state.category].findIndex(obj => obj.name === state.avatarF[state.category].name);
-      return {...state, gender: 'F', itemIndex: initItemIndex }
+      return {...state, gender: 'F', itemIndex: state.choicesF[state.category].findIndex(obj => obj.name === state.avatarF[state.category].name) }
 
     case 'setCategory':
       category = action.payload;
@@ -51,53 +59,53 @@ const reducer = (state, action) => {
       return {...state, category: action.payload, itemIndex: initItemIndex}
 
     case 'prevItem':
-      index = state.itemIndex;
       itemCount = state.gender === 'M' ? state.choicesM[state.category].length : state.choicesF[state.category].length;
-      newIndex = calculateNewIndex(index, itemCount, false)
-
+      newIndex = calculateNewIndex(state.itemIndex, itemCount, false)
       return {...state, itemIndex: newIndex}
 
     case 'nextItem':
-      index = state.itemIndex;
       itemCount = state.gender === 'M' ? state.choicesM[state.category].length : state.choicesF[state.category].length;
-      newIndex = calculateNewIndex(index, itemCount, true)
+      newIndex = calculateNewIndex(state.itemIndex, itemCount, true)
       return {...state, itemIndex: newIndex}
 
-    // case 'equipItem':
-    //   const category = Object.keys(action.payload)[0];
-    //   const item = Object.values(action.payload)[0];
-    //   const newState = { ...state };
-    //   if (state.gender === 'M') {
-    //     newState.avatarM[category] = item
-    //   } else {
-    //     newState.avatarF[category] = item
-    //   }
-    //   return newState;
+    case 'equipItem':
+      const { colors, name, type } = state[`choices${state.gender}`][state.category][state.itemIndex];
+      const itemToEquip = {colors: colors, name: name, type: type};
+      return {
+        ...state,
+        [`avatar${state.gender}`]: {
+          ...state[`avatar${state.gender}`],
+          [state.category]: itemToEquip 
+        }
+      };
 
     default: return state;
   }
 }
 
-export const AvatarBuilder = () => {
+export const AvatarBuilder = (avatarData=null) => {
   const [state, dispatch] = useReducer(reducer, initial);
 
+  const gender = state.gender;
+  
+  const [avatar, choices, category, itemIndex] = state.gender === 'M' ? [state.avatarM, state.choicesM, state.category, state.itemIndex] : [state.avatarF, state.choicesF, state.category, state.itemIndex];
+  
+  const categories = Object.keys(choices);
+  
+  const items = choices[category];
+
+  const palette = avatarPalettes[category];
+  
+  const item = items[itemIndex];
+  
   const setMale = () => dispatch({type: 'setMale'});
   const setFemale = () => dispatch({type: 'setFemale'});
   const setCategory = (payload) => dispatch({type: 'setCategory', payload: payload})
   const prevItem = () => dispatch({type: 'prevItem'});
   const nextItem = () => dispatch({type: 'nextItem'});
-  // const equipItem = (payload) => dispatch({type: 'equipItem', payload: payload});
-
-  const gender = state.gender;
-
-  const [avatar, choices, category, itemIndex] = state.gender === 'M' ? [state.avatarM, state.choicesM, state.category, state.itemIndex] : [state.avatarF, state.choicesF, state.category, state.itemIndex];
-
-  const categories = Object.keys(choices);
-  const items = choices[category];
-
-  const pallete = category === 'skin' ? SKINCOLOR : COLORS;
-
-  const item = items[itemIndex];
-
-  return { avatar, choices, gender, categories, category, items, itemIndex, item, pallete, setMale, setFemale, setCategory, prevItem, nextItem };
+  const equipItem = () => dispatch({type: 'equipItem'});
+  
+  item['colors'] = state.currentColors[category].slice(0, item.scheme);
+  
+  return { avatar, choices, gender, categories, category, items, itemIndex, item, palette, setMale, setFemale, setCategory, prevItem, nextItem, equipItem };
 }
