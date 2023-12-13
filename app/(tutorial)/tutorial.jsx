5e55f3ec8c84beeaede6f52171c1dfb2b16bbe14
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-12-12 20:04:07 -0700

    Add avatar builder rendering, category tabs, inputs

--- app/(tutorial)/tutorial.jsx
+++ app/(tutorial)/tutorial.jsx
@@ -1,19 +1,20 @@
 import { View, ScrollView, Pressable, Text, SafeAreaView } from "react-native";
 import { Stack } from "expo-router";
 import { FONT, SIZES, COLORS, SHADOWS } from "../../constants/theme";
-import { Avatar } from "../../components/avatar";
-import { Foundation, AntDesign } from "@expo/vector-icons";
+import { Avatar, adjustColor } from "../../components/avatar";
+import { Foundation, AntDesign, MaterialCommunityIcons } from "@expo/vector-icons";
 import { useSession, useTheme } from "../../global";
 import { AvatarBuilder } from "../../components/avatar/builder";
-import { useState } from "react";
 import AvatarIcon from "../../components/avatar/icons/icons";
 import { Tab } from "@rneui/themed";
+import { PressableOpacity } from "../../components/common/button";
 
 
 const TutorialView = () => {
   const { theme } = useTheme();
   const { session } = useSession();
   const builder = AvatarBuilder();
+  
 
   return (
     <SafeAreaView style={{flex: 1}}>
@@ -25,9 +26,9 @@ const TutorialView = () => {
         headerTitleStyle: { fontFamily: FONT.cute, fontSize: SIZES.xxLarge, color: theme.highlight,  },
         headerTitleAlign: "center"
       }} />
-      <ScrollView>
 
         {/* SET GENDER */}
+      <View style={{flex: 1, alignItems: "center"}}>
         <View style={{alignItems: "center"}}>
           <View style={[{flexDirection: "row", marginTop: 30, borderRadius: 15, width: 160, height: 65, padding: 1}, SHADOWS.medium]}>
             <Pressable style={{flexGrow: 1}} onPress={builder.setMale} disabled={builder.gender === 'M' ? true : false}>
@@ -69,42 +70,63 @@ const TutorialView = () => {
         </View>
 
         {/* RENDER AVATAR */}
-        <View style={{width: "100%", maxHeight: 300}}>
-          <Avatar avatar={{...builder.avatar, [builder.category]: builder.avatar[builder.category]}} />
+        <View style={{width: "100%", aspectRatio: 1, maxHeight: 300}}>
+          <Avatar avatar={{...builder.avatar, [builder.category]: builder.item}} />
         </View>
 
 
-        <View style={{flexDirection: "row", justifyContent: "space-evenly", width: "100%", display: "none"}}>
-          <View style={{width: "50%", alignItems: "center"}}>
-            <Pressable onPress={null}>
-              <AntDesign name="caretup" size={40} color={theme.primary} />
-            </Pressable>
-            
-            <Text style={{fontSize: 40, fontFamily: FONT.cute}}>{builder.category[0].toUpperCase() + builder.category.slice(1)}</Text>
+        <View style={{ width: "100%", flex: 1 }}>
+          <View style={{justifyContent: "space-evenly", alignItems: "center", flexDirection: "row", width: "100%" }}>
+            <PressableOpacity onPress={builder.prevItem}>
+              <AntDesign name="caretleft" size={30} color={COLORS.ltYellow} />
+            </PressableOpacity>
 
-            <Pressable onPress={null}>
-              <AntDesign name="caretdown" size={40} color={theme.primary} />
-            </Pressable>
+            <Text style={{fontSize: 40, fontFamily: FONT.cute}}>
+              {builder.category[0].toUpperCase()+builder.category.slice(1)}{builder.items.length > 1 ? ` ${builder.itemIndex + 1}` : null}
+            </Text>
+
+            <PressableOpacity onPress={builder.nextItem}>
+              <AntDesign name="caretright" size={30} color={COLORS.ltYellow} />
+            </PressableOpacity>
           </View>
 
-          <View style={{width: "50%", alignItems: "center"}}>
-            <Pressable onPress={builder.prevItem}>
-              <AntDesign name="caretup" size={40} color={theme.primary} />
-            </Pressable>
-
-            <Text style={{fontSize: 40, fontFamily: FONT.cute}}>{builder.itemIndex}</Text>
-
-            <Pressable onPress={builder.nextItem}>
-              <AntDesign name="caretdown" size={40} color={theme.primary} />
-            </Pressable>
+          <View style={{ width: "100%", flex: 1, flexDirection: "row", alignItems: "center", justifyContent: "center" }}>
+            { builder.item.scheme ? builder.item.colors.map((color, index) => (
+              <View key={index} style={{alignItems: "center"}}>
+                <Text style={{ fontFamily: FONT.cute, fontSize: 36 }}>
+                  Color{builder.item.colors.length > 1 ? ` ${index}` : ''}
+                </Text>
+                <PressableOpacity>
+                  <View style={{ height: 40, justifyContent: "center" }}>
+                    <View style={{backgroundColor: color, height: 28, width: 28, margin: 4, borderRadius: 3, borderWidth: 3, borderColor: adjustColor(color)}} />
+                  </View>
+                </PressableOpacity>
+              </View>
+            )) : (
+              <View style={{alignItems: "center"}}>
+                <Text style={{fontFamily: FONT.cute, fontSize: 36}}>Color</Text>
+                <View style={{height: 40, justifyContent: "center"}}>
+                  <MaterialCommunityIcons name="checkbox-blank-off-outline" size={38} color={COLORS.gray} />
+                </View>
+              </View>
+            )}
           </View>
         </View>
-      </ScrollView>
+      </View>
+
+      <PressableOpacity
+      style={{position: "absolute", bottom: 75, right: 25}}
+      onPress={() => builder.equipItem()}>
+        <View style={{height: 50, width: 50, borderRadius: 50, backgroundColor: theme.tertiary, alignItems: "center", justifyContent: "center"}}>
+          <Text style={{fontFamily: FONT.cute, fontSize: 24, color: theme.highlight}}>EQUIP</Text>
+        </View>
+      </PressableOpacity>
+
       <Tab
         value={builder.categories.indexOf(builder.category)}
         onChange={(e) => builder.setCategory(builder.categories[e])}
         indicatorStyle={{
-          backgroundColor: COLORS.dkGreen,
+          backgroundColor: theme.selected,
           height: 3,
           margin: 0,
           padding: 0,
@@ -115,7 +137,7 @@ const TutorialView = () => {
         { builder.categories.map((val, index) => (
           <Tab.Item
             key={index}
-            icon={(active) => <AvatarIcon category={val} color={active ? COLORS.green : theme.highlight} style={{width: "100%", height: 30}} />}
+            icon={(active) => <AvatarIcon category={val} color={active ? theme.selected : theme.highlight} style={{width: "100%", height: 30}} />}
             buttonStyle={(active) => ({
               paddingVertical: 12
             })}
