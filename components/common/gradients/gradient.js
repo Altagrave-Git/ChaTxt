12496dbf61086650b37b1064386db9d2183a7362
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-12-25 03:27:02 -0700

    Optimize svg id generator

--- components/common/gradients/gradient.js
+++ components/common/gradients/gradient.js
@@ -1,18 +1,26 @@
+import React, { useState, useCallback } from "react";
 import Svg, { LinearGradient, Rect, Stop, Defs } from "react-native-svg";
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 
-const SVGGradient = ({colorA, colorB, x1, y1, x2, y2}) => {
-  const svgID = `G${Math.random()}`.split(".").join('-');
+const SVGGradient = ({ colorA, colorB, x1, y1, x2, y2 }) => {
+  const [svgID, setSvgID] = useState(`G${Math.random()}`.split(".").join("-"));
+
+  const onLayout = useCallback(() => {
+    setSvgID(`G${Math.random()}`.split(".").join("-"));
+  }, []);
+
   return (
-    <Svg height="100%" width="100%" style={StyleSheet.absoluteFillObject}>
-      <Defs>
-        <LinearGradient id={svgID} x1={x1} y1={y1} x2={x2} y2={y2} >
-          <Stop offset="0" stopColor={ colorA } />
-          <Stop offset="1" stopColor={ colorB } />
-        </LinearGradient>
-      </Defs>
-      <Rect width="100%" height="100%" fill={`url(#${svgID})`} />
-    </Svg>
+    <View style={StyleSheet.absoluteFill} onLayout={onLayout}>
+      <Svg style={StyleSheet.absoluteFillObject}>
+        <Defs>
+          <LinearGradient id={svgID} x1={x1} y1={y1} x2={x2} y2={y2} >
+            <Stop offset="0" stopColor={ colorA } />
+            <Stop offset="1" stopColor={ colorB } />
+          </LinearGradient>
+        </Defs>
+        <Rect width="100%" height="100%" fill={`url(#${svgID})`} />
+      </Svg>
+    </View>
   )
 };
 
