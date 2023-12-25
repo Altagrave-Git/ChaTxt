63040758b7e4768bf955974bf7b1f49c26dc034f
Damon Turcotte <damon.j.turcotte@gmail.com>
2023-12-25 03:25:31 -0700

    Add to exports: avatarbuilder, itemswiper

--- components/avatar/index.js
+++ components/avatar/index.js
@@ -1,10 +1,14 @@
 import Avatar, { AvatarItem, getAvatarItemSet, baseAvatar } from "./avatar";
 import { adjustColor } from "../../utils/shade";
+import { AvatarBuilder } from "./builder";
+import ItemSwiper from "./interface/swiper/swiper";
 
 export {
   Avatar,
   adjustColor,
   AvatarItem,
   getAvatarItemSet,
-  baseAvatar
+  baseAvatar,
+  AvatarBuilder,
+  ItemSwiper
 }
