33d1c8e941f2d919ff1b3edf3e5701724f6242f2
Damon Turcotte <damon.j.turcotte@gmail.com>
2024-06-19 00:39:57 -0600

    Update import for storage hook

--- global/theme.js
+++ global/theme.js
@@ -1,5 +1,5 @@
 import { createContext, useContext } from 'react';
-import { useStorageState } from "./store";
+import { useStorageState } from "./storage";
 import { THEMES } from "../constants/theme";
 
 const ThemeContext = createContext();
