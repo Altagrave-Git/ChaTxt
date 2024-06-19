2c61d9bf593e05e6a7e8d067b5b3faed291896a6
Damon Turcotte <damon.j.turcotte@gmail.com>
2024-06-19 00:37:26 -0600

    Refactor API class to use querystring utility and handle response consistently

--- api/api.js
+++ api/api.js
@@ -1,11 +1,11 @@
 import { REACT_APP_SERVER_URL } from '@env';
+import { queryString } from '../utils/querystring';
 
 class BaseToonAPI {
   constructor() {
     this.baseUrl = REACT_APP_SERVER_URL;
     this.handleResponse = async (response) => {
       const data = await response.json();
-      
       return {
         status: response.status,
         data: data
@@ -13,36 +13,36 @@ class BaseToonAPI {
     };
   }
 
-  get = async (endpoint, params={}) => {
-    const paramList = [];
-    for (let param in params) {
-      paramList.push(`${param}=${params[param]}`);
+  get = async (path, params={}, token=null) => {
+    const paramString = queryString(params);
+    const options = {
+      method: "GET",
+      credentials: "include"
     }
-    const response = await fetch(`${this.baseUrl}${endpoint}${paramList.length ? `?${paramList.join('&')}`: ''}`)
-      .then(response => {
-        return response.json();
-      })
-      .then(data => {
-        return data;
-      })
-      .catch(error => {
-        console.log(error);
+    if (token) {
+      options.headers = {
+        Authorization: `Token ${token}`
       }
-    );
-    return response;
+    }
+    const response = await fetch(`${this.baseUrl}${path}${paramString}`, options)
+    return this.handleResponse(response);
   }
 
-  post = async (endpoint, body={}, params={}) => {
-    const paramList = [];
-    let status;
-    for (let param in params) {
-      paramList.push(`${param}=${params[param]}`);
-    }
-    const response = await fetch(`${this.baseUrl}${endpoint}${paramList.length ? `?${paramList.join('&')}`: ''}`, {
+  post = async (path, body={}, contentType=null, token=null) => {
+    const options = {
       method: "POST",
       credentials: "include",
       body: body
-    })
+    }
+    const headers = {};
+    if (token) {
+      headers.Authorization = `Token ${token}`;
+    }
+    if (contentType) {
+      headers["Content-Type"] = contentType;
+    }
+    options.headers = headers;
+    const response = await fetch(`${this.baseUrl}${path}`, options)
     return this.handleResponse(response);
   }
 }
