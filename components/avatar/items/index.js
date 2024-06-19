58fce23a8dba2d69739753465bf731f74e634e91
Damon Turcotte <damon.j.turcotte@gmail.com>
2024-06-19 00:42:09 -0600

    Remove avatar palettes, normalize avatar items object

--- components/avatar/items/index.js
+++ components/avatar/items/index.js
@@ -1,41 +1,15 @@
-import { skin as skinItem } from "./skin";
-import { eyes as eyesItem } from "./eyes";
-import { mouth as mouthItem } from "./mouth";
-import { hair as hairItem } from "./hair";
-import { torso as torsoItem } from "./torso";
-import { legs as legsItem } from "./legs";
-import { feet as feetItem } from "./feet";
-import { hands as handsItem } from "./hands";
+import body from "./body";
+import face from "./face";
+import head from "./head";
+import upper from "./upper";
+import lower from "./lower";
 
-
-export const avatarItems = {
-  skin: skinItem,
-  eyes: eyesItem,
-  mouth: mouthItem,
-  hair: hairItem,
-  torso: torsoItem,
-  legs: legsItem,
-  feet: feetItem,
-  hands: handsItem,
+const avatarItems = {
+  body,
+  face,
+  head,
+  upper,
+  lower
 }
 
-import { Skin as skinPalette } from "./skin/skin.styles";
-import { Eyes as eyesPalette } from "./eyes/eyes.styles";
-import { Mouth as mouthPalette } from "./mouth/mouth.styles";
-import { Hair as hairPalette } from "./hair/hair.styles";
-import { Torso as torsoPalette } from "./torso/torso.styles";
-import { Legs as legsPalette } from "./legs/legs.styles";
-import { Hands as handsPalette } from "./hands/hands.styles";
-import { Feet as feetPalette } from "./feet/feet.styles";
-
-
-export const avatarPalettes = {
-  skin: skinPalette,
-  eyes: eyesPalette,
-  mouth: mouthPalette,
-  hair: hairPalette,
-  torso: torsoPalette,
-  legs: legsPalette,
-  feet: feetPalette,
-  hands: handsPalette,
-}
+export default avatarItems;
