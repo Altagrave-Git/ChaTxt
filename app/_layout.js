30bf71b8dbb7fbfbae3017e739835107d3515a26
Damon Turcotte <damon.j.turcotte@gmail.com>
2024-06-19 00:37:54 -0600

    Add APIProvider to _layout.js for global API access

--- app/_layout.js
+++ app/_layout.js
@@ -1,13 +1,15 @@
 import { Slot } from "expo-router";
-import { SessionProvider, ThemeProvider } from "../global";
+import { SessionProvider, ThemeProvider, APIProvider } from "../global";
 
 export default Root = () => {
 
   return (
-    <SessionProvider>
-        <ThemeProvider>
-          <Slot />
-      </ThemeProvider>
-    </SessionProvider>
+      <SessionProvider>
+          <ThemeProvider>
+            <APIProvider>
+              <Slot />
+            </APIProvider>
+        </ThemeProvider>
+      </SessionProvider>
   );
 }
